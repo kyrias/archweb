7b1951047df24ea1ce9706e6982c47139db79d6b
Johannes Löthberg <johannes@kyriasis.com>
2018-01-01 22:25:02 +0100

    Change urls.py from patterns to list of url()
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- releng/urls.py
+++ releng/urls.py
@@ -1,36 +1,33 @@
-from django.conf.urls import include, patterns
+from django.conf.urls import include, url
 
 from .views import ReleaseListView, ReleaseDetailView
+import views
 
-feedback_patterns = patterns('releng.views',
-    (r'^$',                              'test_results_overview', {}, 'releng-test-overview'),
-    (r'^submit/$',                       'submit_test_result', {}, 'releng-test-submit'),
-    (r'^thanks/$',                       'submit_test_thanks', {}, 'releng-test-thanks'),
-    (r'^iso/(?P<iso_id>\d+)/$',          'test_results_iso', {}, 'releng-results-iso'),
-    (r'^(?P<option>.+)/(?P<value>\d+)/$','test_results_for', {}, 'releng-results-for'),
-    (r'^iso/overview/$',                 'iso_overview', {}, 'releng-iso-overview'),
-)
+feedback_patterns = [
+    url(r'^$', views.test_results_overview, name='releng-test-overview'),
+    url(r'^submit/$', views.submit_test_result, name='releng-test-submit'),
+    url(r'^thanks/$', views.submit_test_thanks, name='releng-test-thanks'),
+    url(r'^iso/(?P<iso_id>\d+)/$', views.test_results_iso, name='releng-results-iso'),
+    url(r'^(?P<option>.+)/(?P<value>\d+)/$', views.test_results_for, name='releng-results-for'),
+    url(r'^iso/overview/$', views.iso_overview, name='releng-iso-overview'),
+]
 
-releases_patterns = patterns('releng.views',
-    (r'^$',
-        ReleaseListView.as_view(), {}, 'releng-release-list'),
-    (r'^json/$',
-        'releases_json', {}, 'releng-release-list-json'),
-    (r'^(?P<version>[-.\w]+)/$',
-        ReleaseDetailView.as_view(), {}, 'releng-release-detail'),
-    (r'^(?P<version>[-.\w]+)/torrent/$',
-        'release_torrent', {}, 'releng-release-torrent'),
-)
+releases_patterns = [
+    url(r'^$', ReleaseListView.as_view(), name='releng-release-list'),
+    url(r'^json/$', views.releases_json, name='releng-release-list-json'),
+    url(r'^(?P<version>[-.\w]+)/$', ReleaseDetailView.as_view(), name='releng-release-detail'),
+    url(r'^(?P<version>[-.\w]+)/torrent/$', views.release_torrent, name='releng-release-torrent'),
+]
 
-netboot_patterns = patterns('releng.views',
-    (r'^archlinux\.ipxe$', 'netboot_config', {}, 'releng-netboot-config'),
-    (r'^$', 'netboot_info', {}, 'releng-netboot-info')
-)
+netboot_patterns = [
+    url(r'^archlinux\.ipxe$', views.netboot_config, name='releng-netboot-config'),
+    url(r'^$', views.netboot_info, name='releng-netboot-info')
+]
 
-urlpatterns = patterns('',
-    (r'^feedback/', include(feedback_patterns)),
-    (r'^releases/', include(releases_patterns)),
-    (r'^netboot/', include(netboot_patterns)),
-)
+urlpatterns = [
+    url(r'^feedback/', include(feedback_patterns)),
+    url(r'^releases/', include(releases_patterns)),
+    url(r'^netboot/', include(netboot_patterns)),
+]
 
 # vim: set ts=4 sw=4 et:
